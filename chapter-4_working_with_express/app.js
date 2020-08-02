f1e6ffd03535f1319d28d45a227dcbd97bfdce7e
agungTuanany <agung.tuanany@gmail.com>
2020-08-02 10:11:27 +0700

    Chapter-4: Working with ExpressJS | Installing Express

---
+++ chapter-4_working_with_express/app.js
@@ -0,0 +1,17 @@
+// Core Dependencies
+const http = require("http")
+
+// 3rd Dependencies
+const express = require("express")
+
+// Internal Dependencies
+//...
+
+
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen = (8088);
+
+
