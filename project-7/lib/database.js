dc88d0f7a574de96b0426a0ac4751613233ba1b2
agungTuanany <agung.tuanany@gmail.com>
2020-09-01 01:38:37 +0700

    Chapter-11: Working with NoSQL using MongoDB | project-7: connecting MongoDB into database server

--- project-7/lib/database.js
+++ project-7/lib/database.js
@@ -1,11 +1,16 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-//     // @TODO: move this credential into .env
-const sequelize = new Sequelize("node_complete", "daun", "daun", {
-    dialect: "mariadb",
-    host: "localhost",
-    timezone: "Etc/GMT-7",
-    logging: false
-});
+const mongoConnect = callback => {
 
-module.exports = sequelize;
+    // @TODO: move this credential into .env
+    MongoClient.connect("mongodb+srv://daun:WW2thoti3v9mphPW@udemy-nodejs-maximillia.tz0sa.mongodb.net/udemy-nodejs-maximillian?retryWrites=true&w=majority", {useUnifiedTopology: true})
+        .then((client) => {
+            console.log("Success connect with MongoDB database")
+            callback(client);
+        })
+        .catch(err => console.log(err));
+};
+
+module.exports = mongoConnect;
+
