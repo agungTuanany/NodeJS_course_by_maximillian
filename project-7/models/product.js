d404becdbf008144e2774f520fe477c1903218bc
agungTuanany <agung.tuanany@gmail.com>
2020-09-02 05:28:12 +0700

    Chapter-11: Working with NoSQL using MongoDB | project-7:  Fixing the Add Product Functionality | save() method with argumnet "this._id" is always defined

--- project-7/models/product.js
+++ project-7/models/product.js
@@ -30,7 +30,7 @@ class Product {
         this.price = price;
         this.imageUrl = imageUrl;
         this.description = description;
-        this._id = new mongodb.ObjectId(id);
+        this._id = id ? new mongodb.ObjectId(id) : null;
     };
 
     save() {
