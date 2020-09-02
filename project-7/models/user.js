7662f367ecb4c71b1ac3b6aabf191097df348375
agungTuanany <agung.tuanany@gmail.com>
2020-09-02 07:20:11 +0700

    Chapter-11: Working with NoSQL using MongoDB | project-7: Working on Cart Items. Using "embedded" relations technique

--- project-7/models/user.js
+++ project-7/models/user.js
@@ -25,11 +25,13 @@ const { getDb } = require("./../lib/database.js");
 const ObjectId = mongodb.ObjectId;
 
 class User {
-    constructor(firstName, lastName, email) {
+    constructor(firstName, lastName, email, cart, id) {
 
         this.firstName = firstName;
         this.lastName  = lastName;
         this.email     = email;
+        this.cart      = cart; // {items: []}
+        this._id       = id;
     };
 
     save() {
@@ -38,7 +40,35 @@ class User {
         return db.collection("users")
             .insertOne(this)
             .then(result => {
+
                 console.log("=====> model user.js| succeeded create new user ",result)
+            })
+            .catch(err => console.log(err));
+    };
+
+    addToCart(product) {
+
+        // const cartProduct = this.cart.items.findIndex(cartProductArray => {
+
+        //     return cartProductArray._id === product._id;
+        // });
+
+        // Create new element on the fly
+        // product.quantity = 1;
+
+        // Check if 'cart' have property named 'quantity' in MongoDB documents.
+        // If not just add on the fly new field named 'quantity'.
+        const updatedCart = { items: [{ ...product, quantity: 1 }] };
+        const db = getDb();
+
+        return db.collection("users")
+            .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: updatedCart } } // overwrite the 'old cart' with 'new cart' value
+            )
+            .then(result => {
+
+                console.log(result);
             })
             .catch(err => console.log(err));
     };
@@ -49,6 +79,7 @@ class User {
         return db.collection("users")
             .findOne({ _id: new ObjectId(userId) })
             .then(user => {
+
                 console.log("=====> model user.js| succeeded find user ", user);
                 return user;
             })
