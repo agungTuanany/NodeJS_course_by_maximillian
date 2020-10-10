47d0a5743b7d7e425c13da51e3f33d672a5a1971
agungTuanany <agung.tuanany@gmail.com>
2020-10-10 15:39:36 +0700

    Chapter-18: Error Handling | project-14: Init setup

---
+++ project-14/routes/admin.js
@@ -0,0 +1,76 @@
+"use strict";
+
+/*
+ * A router for admin | as system administrator
+ *
+ */
+
+
+// Core Dependencies
+const path = require("path");
+
+// 3rd party Dependencies
+const express = require("express");
+const { body } = require("express-validator");
+
+// Internal Dependencies
+const adminController = require("../controllers/admin.js");
+const isAuth = require("../middleware/is-auth.js");
+
+// Global variables
+const router = express.Router();
+
+router.get("/products", isAuth, adminController.getProducts);
+
+router.get("/add-product", isAuth, adminController.getAddProduct);
+
+router.post("/add-product",
+    [
+        body("title")
+            .isString()
+            .isLength({ min: 3 })
+            .trim()
+            .withMessage("The title must at least 3 characters"),
+        body("imageUrl")
+            .isURL()
+            .withMessage("Please enter the correct URL"),
+        body("price")
+            .isFloat()
+            .withMessage("Please enter the correct price"),
+        body("description")
+            .isLength({ min: 5, max: 400 })
+            .trim()
+            .withMessage("Description must at least 5 characters")
+    ],
+    isAuth,
+    adminController.postAddProduct
+);
+
+router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
+
+router.post("/edit-product",
+    [
+        body("title")
+            .isString()
+            .isLength({ min: 3 })
+            .trim()
+            .withMessage("The title must at least 3 characters"),
+        body("imageUrl")
+            .isURL()
+            .withMessage("Please enter the correct URL"),
+        body("price")
+            .isFloat()
+            .withMessage("Please enter the correct price"),
+        body("description")
+            .isLength({ min: 5, max: 400 })
+            .trim()
+            .withMessage("Description must at least 5 characters")
+    ],
+    isAuth,
+    adminController.postEditProduct
+);
+
+// @TODO: Change with method DELETE
+router.post("/delete-product", isAuth, adminController.postDeleteProduct);
+
+module.exports = router;
---
+++ project-14/routes/shop.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/*
+ * A router for users | client side.
+ *
+ */
+
+// Core Dependencies
+const path = require("path");
+
+// 3rd party Dependencies
+const express = require("express");
+
+// Internal Dependencies
+const shopController = require("../controllers/shop.js");
+const isAuth = require("../middleware/is-auth.js");
+
+// Global variables
+const router = express.Router();
+
+router.get("/", shopController.getIndex);
+
+router.get("/products", shopController.getProducts);
+
+router.get("/products/:productId", shopController.getProduct);
+
+router.get("/cart", isAuth, shopController.getCart);
+
+router.post("/cart", isAuth, shopController.postCart);
+
+router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
+
+router.post("/create-order", isAuth, shopController.postOrder);
+
+router.get("/orders", isAuth, shopController.getOrders);
+
+// router.get("/checkout", shopController.getCheckout);
+
+module.exports = router;
