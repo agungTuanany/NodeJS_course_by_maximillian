deba421d7df5e4aef5deec496c3498d6b6ac1e40
agungTuanany <agung.tuanany@gmail.com>
2020-10-21 22:44:39 +0700

    Chapter-24: Working with REST API's - The practical App | project-20: Starting with User Login

--- project-20/server/controllers/auth.js
+++ project-20/server/controllers/auth.js
@@ -32,8 +32,6 @@ const signup = (request, response, next) => {
                 password: hashedPassword
             });
 
-            console.log("===> User [2]", user);
-
             return user.save();
         })
         .then(result => {
@@ -48,6 +46,48 @@ const signup = (request, response, next) => {
         .catch(err => {
 
             if (!err.statusCode) {
+            console.log("===> auth signup error:", err);
+                err.statusCode = 500;
+            };
+
+            next(err);
+        });
+};
+
+const login = (request, response, next) => {
+
+    const email = request.body.email;
+    const password = request.body.password;
+    let loadedUser;
+
+    User.findOne({ email: email })
+        .then(user => {
+
+            if (!user) {
+                const error = new Error("A user with this email could not be found");
+                error.statusCode = 401;     // @NOTE: 401 = not authenticated
+
+                throw error;
+            };
+
+            loadedUser = user;
+
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+
+            if (!isEqual) {
+                const error = new Error("Wrong Passowrd!");
+                error.statusCode = 401;
+
+                throw error;
+            };
+
+            // @TODO: JWT authentication
+        })
+        .catch(err => {
+
+            if (!err.statusCode) {
             console.log("===> password bycrpt error:", err);
                 err.statusCode = 500;
             };
@@ -57,6 +97,6 @@ const signup = (request, response, next) => {
 }
 
 module.exports = {
-    signup
-
-}
+    signup,
+    login
+};
