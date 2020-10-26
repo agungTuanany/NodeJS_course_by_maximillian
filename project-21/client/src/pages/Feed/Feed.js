0aac2b1c6b4e08deb1640efb5ecddc4c0751cc74
agungTuanany <agung.tuanany@gmail.com>
2020-10-26 15:20:57 +0700

    Chapter-26: Understanding WebSocket - socket.io | project-21: Identifying Realtime Potential

--- project-21/client/src/pages/Feed/Feed.js
+++ project-21/client/src/pages/Feed/Feed.js
@@ -42,6 +42,24 @@ class Feed extends Component {
     openSocket("http://localhost:8081");;
   };
 
+  addPost = post => {
+
+    this.setState(prevState => {
+
+      const updatedPosts = [...prevState.posts];
+
+      if (prevState.postPage === 1) {
+        updatedPosts.pop();
+        updatedPosts.unshift(post);
+      };
+
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
+    });
+  };
+
   loadPosts = direction => {
 
     if (direction) {
