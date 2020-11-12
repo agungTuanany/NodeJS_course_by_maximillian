02ee842c6ea459ec4c33f7dfc2c992e8d3db1b93
agungTuanany <agung.tuanany@gmail.com>
2020-11-12 20:18:10 +0700

    Chapter-27: Working with GraphQL | project-22: Adding Login Functionality

--- project-22/client/src/App.js
+++ project-22/client/src/App.js
@@ -62,9 +62,21 @@ class App extends Component {
   loginHandler = (event, authData) => {
 
     event.preventDefault();
+
+    const graphqlQuery = {
+      query: `
+        {
+          login(email: "${authData.email}", password: "${authData.password}") {
+            token
+            userId
+          }
+        }
+      `
+    };
+
     this.setState({ authLoading: true });
 
-    fetch("http://localhost:8081/auth/login", {
+    fetch("http://localhost:8081/graphql", {
       method: "POST",
       headers: {
         "Content-type": "application/json"
@@ -76,23 +88,32 @@ class App extends Component {
     })
       .then(res => {
 
-        if (res.status === 422) {
-          throw new Error('Validation failed.');
-        }
-        if (res.status !== 200 && res.status !== 201) {
-          console.log('Error!');
-          throw new Error('Could not authenticate you!');
-        }
+        // if (res.status === 422) {
+        //   throw new Error('Validation failed.');
+        // }
+        // if (res.status !== 200 && res.status !== 201) {
+        //   console.log('Error!');
+        //   throw new Error('Could not authenticate you!');
+        // }
+
         return res.json();
       })
       .then(resData => {
 
+        if (resData.erros && resData.erros[0].status === 422) {
+          throw new Error("Validation failed. Make sure the email address isn't used yet!");
+        };
+
+        if (resData.errors) {
+          throw new Error("User login failed!");
+        };
+
         console.log(resData);
         this.setState({
           isAuth: true,
-          token: resData.token,
+          token: resData.data.login.token,
           authLoading: false,
-          userId: resData.userId
+          userId: resData.data.login.userId
         });
 
         localStorage.setItem('token', resData.token);
